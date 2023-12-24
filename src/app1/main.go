b90ffe1c386b2f877669fc899fbf1c4afe76f740
Liang <chenliang.info@gmail.com>
2023-12-24 09:15:13 -0800

    add proto sample

--- src/app1/main.go
+++ src/app1/main.go
@@ -1,5 +1,11 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"github.com/newcl/bazel/proto/app1"
+)
 func main()  {
 	fmt.Printf("Hello from app1 \n")
+
+	p := app1.Person{}
+	fmt.Printf("%v", p)
 }
