abf84c94975f922a273ba38ea9ad85d00c330069
Liang <chenliang.info@gmail.com>
2023-12-24 20:54:15 -0800

    add viper env&flag sample

--- src/app1/cmd/serve.go
+++ src/app1/cmd/serve.go
@@ -9,6 +9,7 @@ import(
 
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 		"go.uber.org/fx"
 	_ "github.com/rs/zerolog"
     _ "github.com/rs/zerolog/log"
@@ -40,11 +41,27 @@ var serveCmd = &cobra.Command{
 	Short: "short desc",
 	Long: "Long desc",
 	Run:func(cmd *cobra.Command, args []string){
+
+		// APP1_P1=33 bazel run //src/app1/main:main
+viper.SetEnvPrefix("app1")
+viper.BindEnv("p1")
+
+
+
+fmt.Printf("app1 p1 %v\n", viper.Get("p1"))
+fmt.Printf("app1 flag port %v\n", viper.Get("port"))
+
 fx.New(
 fx.Provide(NewHTTPServer),
 fx.Invoke(func(*http.Server){}),
 ).Run()
 	},
+}
+
+func init() {
+	// bazel run //src/app1/main:main -- --port 9999
+	serveCmd.Flags().Int("port", 2222, "my port")
+	viper.BindPFlag("port", serveCmd.Flags().Lookup("port"))
 }
 
 func Execute(){
